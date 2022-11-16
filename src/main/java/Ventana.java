18a1586f70e08d19d1c6fa2658aaa0a3c439aa7f
NicolasAriasUfro <101823845+NicolasAriasUfro@users.noreply.github.com>
2022-11-16 11:22:53 -0300

    botones nombrados

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -4,14 +4,14 @@ public class Ventana extends JFrame{
     Ventana(){
         frameInit();
         this.setContentPane(ventanaConexion);
-        this.setSize(500,500);
+        this.setSize(700,500);
     }
     private JPanel ventanaConexion;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton btmNuevo;
+    private JButton btmGuardar;
+    private JButton btmEliminar;
+    private JButton btmActualizar;
 }
