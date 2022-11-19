fec3fbbeefffb96b57b4d22ea069e68c51216ee9
Sabiomen <sabiomen@gmail.com>
2022-11-18 23:15:40 -0300

    Creacion de base Ventana 2 para la plantacion

---
+++ src/main/java/Ventana_2.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Ventana_2 {
+    private JTextField textField1;
+    private JTextField textField2;
+}
--- src/main/java/to do.java
+++ src/main/java/to do.java
@@ -1,4 +1,3 @@
 //To do:
-//Validacion de ingreso de datos
 //Otra ventana para plantar y otra ver lo plantado(quizas
 //Preparar la presentacion
